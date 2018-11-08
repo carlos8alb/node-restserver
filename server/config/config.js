5b8eb31a57261b2ec930df7d6674fecdd7602dc3
Carlos Albarracin <carlos8_alb@hotmail.com>
2018-11-08 19:44:17 +1300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,23 @@
 // ============================
 
 process.env.PORT = process.env.PORT || 3000;
+
+// ============================
+// Entorno
+// ============================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ============================
+// Entorno
+// ============================
+
+let urlDB;
+
+if ( process.env.NODE_ENV = 'dev' ) {
+	urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+	urlDB = 'mongodb://carlos8_alb:123456abc@ds155313.mlab.com:55313/cafedb-udemy';
+}
+
+process.env.URLDB = urlDB;
